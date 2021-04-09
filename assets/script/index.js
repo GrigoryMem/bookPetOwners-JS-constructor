05c396df8b4f81a192e5444a8958ed9f59fec8f6
Grogory Morozov <morozehc@yandex.ru>
2021-04-09 16:02:29 +0300

    2.insertAdjasmentHTML() first elements

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,11 +1,12 @@
 const model = [
-    {type:"title", value:"hello world"},
-    {type:"intro", value:"some text"},
-    {type:"columns", value:[
+    {type:"title", value:"Hello, world! Название таблицы JS"},
+    {type:"intro", value:"Ввведние в таблицу. Инструкция JS"},
+    {type:"header", value:[
         "Владелец животного",
             "Кличка животного",
         "Возраст животного",
         "Адрес владельца"
+
 
         ]}
 ]
@@ -13,3 +14,36 @@ const model = [
 
 const $site  = document.querySelector("#site");
 console.log($site)
+
+model.forEach(point =>{
+
+    let content = "";
+
+    if(point.type === "title"){
+        content = `
+        <div class="row">
+                <div class="col-sm">
+                    <h1>${point.value}</h1>
+                </div>
+            </div>
+        
+        `
+    } else if(point.type==="intro"){
+        content = `
+        <div class="row">
+                <div class="col-sm">
+                    <p>${point.value}</p>
+                </div>
+            </div>
+        `
+
+    } else if(point.type === "header") {
+
+    }
+
+    console.log(content);
+
+    $site.insertAdjacentHTML("beforeend",content);
+
+
+})
