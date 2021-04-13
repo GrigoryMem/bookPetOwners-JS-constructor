15507c1de8586d846405ef463c1c3eab271efcd1
Grogory Morozov <morozehc@yandex.ru>
2021-04-13 19:28:29 +0300

    9. check point 19:28

--- dist/script.34c90582.js
+++ dist/script.34c90582.js
@@ -199,7 +199,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59851" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59936" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
